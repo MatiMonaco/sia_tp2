b8c66a28a890879c23fef466b9dd4f04094dcaf6
Ignacio Vazquez <40323334+igvazquez@users.noreply.github.com>
2020-08-25 20:34:32 -0300

    Character class

--- src/main/java/ar/edu/itba/Main.java
+++ src/main/java/ar/edu/itba/Main.java
@@ -3,6 +3,10 @@ package ar.edu.itba;
 public class Main {
 
     public static void main(String[] args) {
+        Character warrior = new Character(Character.CharacterClass.WARRIOR);
 
+        System.out.println(warrior.type);
+        System.out.println(warrior.type.getBaseAttackM());
+        System.out.println(warrior.type.getBaseDefenseM());
     }
 }
