9f144745db5d6fb4af0ce5b33ba782bed4cb6bbd
Ignacio Vazquez <40323334+igvazquez@users.noreply.github.com>
2020-08-27 12:49:38 -0300

    ejemplo del parser, ahora lee armas.tsv (hay que sacar la primer linea del archivo que tiene los nombres de las columnas para que no tire error)

--- src/main/java/ar/edu/itba/Main.java
+++ src/main/java/ar/edu/itba/Main.java
@@ -1,27 +1,53 @@
 package ar.edu.itba;
 
+import com.univocity.parsers.common.ParsingContext;
+import com.univocity.parsers.common.processor.ObjectRowProcessor;
+import com.univocity.parsers.conversions.Conversions;
+import com.univocity.parsers.tsv.TsvParser;
+import com.univocity.parsers.tsv.TsvParserSettings;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.knowm.xchart.*;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        JSONParser parser = new JSONParser();
+        JSONParser jsonParser = new JSONParser();
         String path = "./config.json";
+
+        TsvParserSettings settings = new TsvParserSettings();
+        ObjectRowProcessor rowProcessor = new ObjectRowProcessor() {
+            @Override
+            public void rowProcessed(Object[] row, ParsingContext context) {
+                //here is the row. Let's just print it.
+                System.out.println(Arrays.toString(row));
+            }
+        };
+        rowProcessor.convertIndexes(Conversions.toBigDecimal()).set(2, 5);
+        rowProcessor.convertFields(Conversions.trim(), Conversions.toLowerCase()).set("Description", "Model");
+        settings.setRowProcessor(rowProcessor);
+
+        TsvParser parser = new TsvParser(settings);
+        try {
+            parser.parse(new FileReader("./src/main/Resources/armas.tsv"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         try (Reader reader = new FileReader(path)) {
 
-            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
             System.out.println(jsonObject);
 
             String crossing = (String) jsonObject.get("crossing");
